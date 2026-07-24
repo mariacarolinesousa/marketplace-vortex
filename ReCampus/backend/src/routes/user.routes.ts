ca139b06d25992bb758a49710f686bba08366dd1
Maria Caroline Sousa <carolinesousa@tutanota.com>
2026-07-24 10:12:30 -0300

    Update user.routes.ts

--- ReCampus/backend/src/routes/user.routes.ts
+++ ReCampus/backend/src/routes/user.routes.ts
@@ -3,13 +3,11 @@ import { UserController } from "../controllers/UserController";
 import { auth } from "../middlewares/auth";
 
 const router = Router();
-const controller = new UserController();
+const userController = new UserController();
 
 router.get("/me", auth, (req, res) => {
   controller.me(req, res);
 });
-router.get("/me/ads", auth, (req, res) => {
-  controller.myAds(req, res);
-});
+router.get("/me/ads", auth, userController .myAds.bind(userController));
 
 export default router;
