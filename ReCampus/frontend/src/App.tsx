9749b1b4d8f72450f1e2b698268f0a6e02252b87
Maria Caroline Sousa <carolinesousa@tutanota.com>
2026-07-31 20:01:22 -0300

    Corrige login e navegação

--- ReCampus/frontend/src/App.tsx
+++ ReCampus/frontend/src/App.tsx
@@ -6,45 +6,26 @@ import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import CreateAd from "./pages/CreateAd/CreateAd";
 import Landing from "./pages/Landing/Landing";
+import AdDetails from "./pages/AdDetails/AdDetails";
 
 export default function App() {
   return (
     <Routes>
-      
-      <Route
-        path="/"
-        element={<Landing />}
-      />
+      <Route path="/" element={<Landing />} />
 
-      <Route
-        path="/home"
-        element={<Home />}
-      />
+      <Route path="/home" element={<Home />} />
 
-      <Route
-        path="/login"
-        element={<Login />}
-      />
+      <Route path="/login" element={<Login />} />
 
-      <Route
-        path="/register"
-        element={<Register />}
-      />
+      <Route path="/register" element={<Register />} />
 
-      <Route
-        path="/create"
-        element={<CreateAd />}
-      />
+      <Route path="/create" element={<CreateAd />} />
 
-      <Route
-        path="/my-ads"
-        element={<MyAds />}
-      />
+      <Route path="/my-ads" element={<MyAds />} />
 
-      <Route
-        path="/profile"
-        element={<Profile />}
-      />
+      <Route path="/profile" element={<Profile />} />
+
+      <Route path="/ads/:id" element={<AdDetails />} />
     </Routes>
   );
 }
