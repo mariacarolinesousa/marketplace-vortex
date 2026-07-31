68b074b986264321a3e8e3573afcc2a02b93cb1d
Maria Caroline Sousa <carolinesousa@tutanota.com>
2026-07-30 21:29:40 -0300

    atualização
    atualizando

--- ReCampus/frontend/api.ts
+++ ReCampus/frontend/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "http://localhost:3333",
+    "https://marketplace-vortex-ten.vercel.app",
 });
 
 export default api;
