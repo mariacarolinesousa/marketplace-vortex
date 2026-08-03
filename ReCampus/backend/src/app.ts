bc264eda3cb89ff623d8df70d50502d5b322f558
Maria Caroline Sousa <carolinesousa@tutanota.com>
2026-08-03 11:29:10 -0300

    Revert "Adiciona rota inicial do backend"
    This reverts commit f19d115dec837b8ce8aee27769f620dd5190ca71.

--- ReCampus/backend/src/app.ts
+++ ReCampus/backend/src/app.ts
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
 import adRoutes from "./routes/ad.routes.js";
-import userRoutes from "./routes/user.routes.js";
 
 
 const app = express();
@@ -17,20 +16,8 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-app.use("/auth", authRoutes);
-app.use("/ads", adRoutes);
-app.use("/users", userRoutes);
-app.get("/", (_req, res) => {
-  return res.status(200).json({
-    status: "online",
-    message: "Backend do Marketplace Vortex funcionando",
-  });
-});
-app.use(cors());
 
 app.use(express.json());
-
-app.use("/users", userRoutes)
 app.use("/auth", authRoutes);
 app.use("/ads", adRoutes);
 
